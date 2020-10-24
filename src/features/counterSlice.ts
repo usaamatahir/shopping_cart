f9c4da7ea706f6a8a3beaad4b59c55c559a71d16
usaamatahir <usamatahir8330@gmail.com>
2020-10-24 04:27:10 -0700

    Update counterSlice.ts

--- src/features/counterSlice.ts
+++ src/features/counterSlice.ts
@@ -1,53 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
-import {cartType} from '../Types/types';
+import { createSlice } from "@reduxjs/toolkit";
+import { cartType } from "../Types/types";
 
 const initialState: cartType[] = [];
 
 export const cartSlice = createSlice({
-  name: 'Cart',
+  name: "Cart",
   initialState,
   reducers: {
     addToCart: (state, action) => {
       let notFind = false;
-     state.map(product => {
-       if(product._productId !== action.payload._productId) {
-         
-         return product
-       }
-
-       else if(product._productId === action.payload._productId){
-         notFind = true;
-         return {...product, quantity: product.quantity++ }
-       }
-     })
-     if(notFind ===false ){
-        return [...state, action.payload]
+      state.map((product) => {
+        if (product._productId !== action.payload._productId) {
+          return product;
+        } else if (product._productId === action.payload._productId) {
+          notFind = true;
+          return { ...product, quantity: product.quantity++ };
+        }
+      });
+      if (notFind === false) {
+        return [...state, action.payload];
       }
     },
- 
+
     removeFromCart: (state, action) => {
-      return state.filter(product => product._productId !== action.payload._productId)
-      
+      return state.filter(
+        (product) => product._productId !== action.payload._productId
+      );
     },
 
     decreaseFromCart: (state, action) => {
-     state.map(product => {
-       if(product._productId !== action.payload._productId) {
-
-         return product
-       }
-
-       else if(product._productId === action.payload._productId){
-         return {...product, quantity: product.quantity-- }
-       }
-     })
-    }
-    
-    
+      state.map((product) => {
+        if (product._productId !== action.payload._productId) {
+          return product;
+        } else if (product._productId === action.payload._productId) {
+          return { ...product, quantity: product.quantity-- };
+        }
+      });
+    },
   },
 });
 
-export const { addToCart, removeFromCart, decreaseFromCart } = cartSlice.actions;
-
+export const {
+  addToCart,
+  removeFromCart,
+  decreaseFromCart,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
